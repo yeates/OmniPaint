64f7f436a2c2d792cf2721d7b4006f614c4ba230
Ziyun Zeng <151905624+zzzmyyzeng@users.noreply.github.com>
2025-10-18 01:30:17 -0400

    Update CFD Score

--- cfd_score/run_cfd.sh
+++ cfd_score/run_cfd.sh
@@ -1,4 +1,4 @@
 python main.py \
-    --input_path /home/cxu-serve/p62/ytang37/projects/MMComp/OminiPaint-Dev/realworld_val_new/mat_results \
-    --mask_path /home/cxu-serve/p62/ytang37/projects/MMComp/OminiPaint-Dev/realworld_val_new/mask 
+    --input_path <your_input_image_folder_path> \
+    --mask_path <your_mask_image_folder_path>
 
